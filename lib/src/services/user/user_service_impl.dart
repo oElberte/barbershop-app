b41d42b394f631e84d710c58f30d6ecaa27e6169
Elberte Plinio <elbertedoolan@gmail.com>
2023-08-17 19:49:51 -0300

    added user service

---
+++ lib/src/services/user/user_service_impl.dart
@@ -0,0 +1,32 @@
+import 'package:dw_barbershop/src/core/constants/local_storage_keys.dart';
+import 'package:dw_barbershop/src/core/exceptions/auth_exception.dart';
+import 'package:dw_barbershop/src/core/exceptions/service_exception.dart';
+import 'package:dw_barbershop/src/core/fp/either.dart';
+import 'package:dw_barbershop/src/core/fp/nil.dart';
+import 'package:dw_barbershop/src/repositories/user/user_repository.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import './user_service.dart';
+
+class UserServiceImpl implements UserService {
+  final UserRepository userRepository;
+
+  UserServiceImpl({required this.userRepository});
+
+  @override
+  Future<Either<ServiceException, Nil>> login(String email, String password) async {
+    final loginResult = await userRepository.login(email, password);
+
+    switch (loginResult) {
+      case Failure(:final exception):
+        return switch (exception) {
+          AuthError() => Failure(ServiceException('Erro ao realizar login')),
+          AuthUnauthorizedException() => Failure(ServiceException('Login ou senha inválidos')),
+        };
+      case Success(value: final accessToken):
+        final sp = await SharedPreferences.getInstance();
+        sp.setString(LocalStorageKeys.accessToken, accessToken);
+        return Success(nil);
+    }
+  }
+}
