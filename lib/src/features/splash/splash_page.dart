75857eb43d88dca81c5cc77a2c0ff20413ca6ecb
Elberte Plinio <elbertedoolan@gmail.com>
2023-08-20 18:26:39 -0300

    added auto login on splash page

--- lib/src/features/splash/splash_page.dart
+++ lib/src/features/splash/splash_page.dart
@@ -1,20 +1,28 @@
-import 'package:dw_barbershop/src/core/ui/constants.dart';
-import 'package:dw_barbershop/src/features/auth/login/login_page.dart';
-import 'package:flutter/material.dart';
+import 'dart:async';
+import 'dart:developer';
 
-class SplashPage extends StatefulWidget {
+import 'package:dw_barbershop/src/core/ui/constants.dart';
+import 'package:dw_barbershop/src/core/ui/helpers/messages.dart';
+import 'package:dw_barbershop/src/features/splash/splash_vm.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class SplashPage extends ConsumerStatefulWidget {
   const SplashPage({super.key});
 
   @override
-  State<SplashPage> createState() => _SplashPageState();
+  ConsumerState<SplashPage> createState() => _SplashPageState();
 }
 
-class _SplashPageState extends State<SplashPage> {
+class _SplashPageState extends ConsumerState<SplashPage> {
   var _scale = 10.0;
   var _animationOpacityLogo = 0.0;
 
   double get _logoAnimationWidth => 100 * _scale;
   double get _logoAnimationHeight => 120 * _scale;
+
+  var endedAnimation = false;
+  Timer? redirectTimer;
 
   @override
   void initState() {
@@ -27,8 +35,40 @@ class _SplashPageState extends State<SplashPage> {
     });
   }
 
+  void _redirect(String routeName) {
+    if (!endedAnimation) {
+      redirectTimer?.cancel();
+      redirectTimer = Timer(const Duration(milliseconds: 300), () {
+        _redirect(routeName);
+      });
+    } else {
+      redirectTimer?.cancel();
+      Navigator.of(context).pushNamedAndRemoveUntil(routeName, (route) => false);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    ref.listen(splashVmProvider, (_, state) {
+      state.whenOrNull(
+        error: (error, stackTrace) {
+          log('Erro ao validar login', error: error, stackTrace: stackTrace);
+          Messages.showError('Erro ao validar o login', context);
+          _redirect('/auth/login');
+        },
+        data: (data) {
+          switch (data) {
+            case SplashState.loggedAdm:
+              _redirect('/home/adm');
+            case SplashState.loggedEmployee:
+              _redirect('/home/employee');
+            case _:
+              _redirect('/home/login');
+          }
+        },
+      );
+    });
+
     return Scaffold(
       backgroundColor: Colors.black,
       body: DecoratedBox(
@@ -43,21 +83,7 @@ class _SplashPageState extends State<SplashPage> {
           duration: const Duration(seconds: 1),
           curve: Curves.easeIn,
           opacity: _animationOpacityLogo,
-          onEnd: () => Navigator.pushReplacement(
-            context,
-            PageRouteBuilder(
-              settings: const RouteSettings(name: '/auth/login'),
-              pageBuilder: (context, animation, secondaryAnimation) {
-                return const LoginPage();
-              },
-              transitionsBuilder: (_, animation, __, child) {
-                return FadeTransition(
-                  opacity: animation,
-                  child: child,
-                );
-              },
-            ),
-          ),
+          onEnd: () => setState(() => endedAnimation = true),
           child: Center(
             child: AnimatedContainer(
               duration: const Duration(seconds: 1),
